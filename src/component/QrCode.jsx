49c3fe1c9e5d442d03a69b3f7469fd65b3098e78
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-09-14 23:36:49 +0530

    Add the tailwind config to the project.

--- src/component/QrCode.jsx
+++ src/component/QrCode.jsx
@@ -3,13 +3,19 @@ import { QRCodeCanvas } from "qrcode.react";
 import { QRCodeSVG } from "qrcode.react";
 
 function QrCode({ value }) {
+  const qrCode = React.useMemo(() => {
+    return <QRCodeSVG value={value} size={200} level={"H"} />;
+  }, [value]);
+
+  console.log(qrCode);
+
   return (
     <div>
-      {/* <QRCodeCanvas value={value} /> */}
-      <QRCodeSVG value={value} />
-      <a href="/" download={`${(<QRCodeSVG value={value} />)}.png`}>
+      <QRCodeCanvas value={value} />
+      {/* <QRCodeSVG value={value} /> */}
+      {/* <a href="/" download={`${(<QRCodeSVG value={value} />)}.png`}>
         Download
-      </a>
+      </a> */}
     </div>
   );
 }
