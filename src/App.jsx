73516f2868575ac36780f7ae6299d50cadec39b2
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-09-14 17:21:16 +0530

    Add the Input field of the URL.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useCallback } from "react";
+import { useState } from "react";
+
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInput = useCallback((e) => {
+    e.preventDefault();
+    setInputValue(e.target.elements.url.value);
+  }, []);
+  console.log(inputValue);
+
   return (
     <>
       <h1> QR code Generator</h1>
+      <form onSubmit={handleInput}>
+        <label htmlFor="url">Enter your URL :</label>
+        <input type="url" name="url" id="url" required />
+        <button type="submit">Generate</button>
+      </form>
     </>
   );
 }
