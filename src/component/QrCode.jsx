923d2f0a7165b7c8d8cb19f53d7e9ae7701dad74
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-09-16 16:57:35 +0530

    "Updated QrCode component to use useRef and QRCodeCanvas, added download functionality"

--- src/component/QrCode.jsx
+++ src/component/QrCode.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import { QRCodeCanvas } from "qrcode.react";
-import { QRCodeSVG } from "qrcode.react";
 
 function QrCode({ value, size }) {
-  const qrCode = React.useMemo(() => {
-    return <QRCodeSVG value={value} size={200} level={"H"} />;
-  }, [value]);
-
-  console.log(qrCode);
+  const qrRef = useRef(null);
+  const handleDownload = () => {
+    console.log(qrRef);
+    console.log(qrRef.current);
+    const canvas = qrRef.current.querySelector("canvas");
+    console.log(canvas);
+    const image = canvas.toDataURL("image/png");
+    console.log(image);
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "qrcode.png";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
-    <div>
+    <div ref={qrRef}>
       <QRCodeCanvas value={value} size={size} />
-      {/* <QRCodeSVG value={value} /> */}
-      {/* <a href="/" download={`${(<QRCodeSVG value={value} />)}.png`}>
+      <button
+        onClick={handleDownload}
+        className=" px-4 py-2 bg-[#90AFC5] text-[#101820] mt-6 rounded-lg"
+      >
         Download
-      </a> */}
+      </button>
     </div>
   );
 }
