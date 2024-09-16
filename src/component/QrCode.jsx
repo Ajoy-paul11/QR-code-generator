6ed80194a5ef43e5a504091dd7bacd62477bb2ba
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-09-16 19:36:45 +0530

    "Updated QrCode component to rename handleDownload to handlePNG, add new handlePDF function, and modify button layout and styling"

--- src/component/QrCode.jsx
+++ src/component/QrCode.jsx
@@ -3,7 +3,7 @@ import { QRCodeCanvas } from "qrcode.react";
 
 function QrCode({ value, size }) {
   const qrRef = useRef(null);
-  const handleDownload = () => {
+  const handlePNG = () => {
     console.log(qrRef);
     console.log(qrRef.current);
     const canvas = qrRef.current.querySelector("canvas");
@@ -18,15 +18,35 @@ function QrCode({ value, size }) {
     document.body.removeChild(link);
   };
 
+  const handlePDF = () => {
+    const canvas = qrRef.current.querySelector("canvas");
+    const image = canvas.toDataURL("image/jpg");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "qrcode.jpg";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div ref={qrRef}>
-      <QRCodeCanvas value={value} size={size} />
-      <button
-        onClick={handleDownload}
-        className=" px-4 py-2 bg-[#90AFC5] text-[#101820] mt-6 rounded-lg"
-      >
-        Download
-      </button>
+      <QRCodeCanvas value={value} size={size} className=" mx-auto" />
+      <div className=" flex justify-center items-center gap-x-6 mt-6 text-2xl lg:text-3xl">
+        Download :
+        <button
+          onClick={handlePNG}
+          className=" px-4 py-2 bg-[#90AFC5] text-[#101820] rounded-lg text-xl"
+        >
+          png
+        </button>
+        <button
+          onClick={handlePDF}
+          className=" px-4 py-2 bg-[#90AFC5] text-[#101820] rounded-lg text-xl"
+        >
+          jpg
+        </button>
+      </div>
     </div>
   );
 }
